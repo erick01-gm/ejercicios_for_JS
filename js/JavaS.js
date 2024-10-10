0370e7c00676d80b47e00ad8df86cc887f4f4ba2
Erick <erickgm2600@gmail.com>
2024-10-10 14:03:15 -0600

    Add files via upload

---
+++ js/JavaS.js
@@ -0,0 +1,17 @@
+/*
+realizar un programa en javascript que muestre los numeros del 1-35
+(uno bajo el otro)mediante un concole.log. Utilizar para esto alguna 
+estructura repetitva 
+*/
+for(var i=1; i<=35; i++){
+    console.log(i); 
+}
+/*
+Realizar en JavaScript la cuenta regresica para el a;o nuevo, comenzando
+10 y terminando en 0. Al finalizar, debe mostrarse el mensaje feliza a;o
+nuevo
+*/
+for (var i=10; i>=1; i--){
+    console.log('Quedan '+i+ ' segundos'); 
+}
+console.log('Feliz Año Nuevo!!');
